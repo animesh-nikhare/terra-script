942b8d276436e9576e36808aa532128092e72d66
animesh-nikhare <animesh.nikhare97@gmail.com>
2024-11-14 14:59:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,18 @@
 provider "aws" {
-  region = "us-west-1"
+region = "us-west-1"
 }
 
-resource "aws_instance" "example" {
-  ami = "ami-0175bdd48fdb0973b"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "terra-jenkins"
-    }
+resource "aws_instance" "one" {
+count = 4
+ami = "ami-0a9271c49701613c1"
+instance_type = "t2.medium"
+key_name = "N.C-key"
+vpc_security_group_ids = ["sg-07d74320ae4e56d16"]
+tags = {
+Name = var.instance_names[count.index]
+}
+}
+
+variable "instance_names" {
+default = ["Ansible", "Dev", "Test", "Monitoring"]
 }
